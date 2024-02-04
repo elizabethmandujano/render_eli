920f137adf232a31ae326dd011dff7279e010245
elizabethmandujano <158283810+elizabethmandujano@users.noreply.github.com>
2024-02-04 19:16:59 +0400

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
 import streamlit as st
 
 st.header('Lanzar una moneda')
-st.write('Esta aplicación aún no es funcional. En construcción.')
+
+number_of_trials = st.slider ('¿Cuántos intentos quieres?' , 1, 1000, 10)
+start_button = st.button( 'Empezar')
+
+if start_button:
+  st.write(f'Experimento con {number_of_trials} intentos en curso.')
+
+st.write ('Esta aplicación aún no es funcional. Sigue en contrucción... disculpe la demora jiji')
